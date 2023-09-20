b43d14a42d3487d75ad3a41a06c0383016b466c1
HariKrishna <vanapamula.harikrishna@gmail.com>
2023-09-20 16:12:44 +0530

    Added test cases

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,32 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    public function test_app(): void
+    {  
+        $response = $this->get('/')
+            ->assertRedirect('/books');
+        $response->assertStatus(302);
+    }
 
-        $response->assertStatus(200);
+    public function test_book_home(): void
+    {  
+        $response = $this->get('/books');
+        $response->assertSeeText('Books');
+    }
+
+    public function test_book_search(): void
+    {  
+        $response = $this->get('/books?title=Mollitia officia possimus.');
+        $response->assertSeeText('Books');
+        $response->assertSeeText('Mollitia officia possimus.');
     }
 }
